55db2338a14ff11c96ed4e990702184cf3bb6963
zayedu <umerzayed1@gmail.com>
2025-01-02 17:58:15 -0500

    feat: complete module 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { Outlet } from 'react-router-dom';
+
 import Devbar from '@/components/Devbar/Devbar';
-import HomePage from '@/pages/HomePage';
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
         <Devbar />
       </div>
       <div className='ml-[700px]'>
-        <HomePage />
+        <Outlet />
       </div>
     </>
   );
